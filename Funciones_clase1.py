9767e8bf2000f90007f798b940fa38022cf50b7e
Luis Eloy Homobono Fuentes <luis.homobono@ciencias.unam.mx>
2019-05-23 01:20:29 -0500

    Actualizado 23-05-19

---
+++ Funciones_clase1.py
@@ -0,0 +1,21 @@
+def resultado(calificacion):
+    if calificacion < 5:
+        print("Estas REPROBADO")
+    else:
+        print("SUERTUDO")
+        
+        
+def sonido_animal(animal):        
+	if animal == "gato":
+    	print("miau")
+	elif animal == "perro":
+    	print("guau")
+	elif animal == "pez":
+    	print ("glub glub")
+	elif animal == "gallo":
+    	print("kikiriki")
+	elif animal == "vaca":
+    	print("muuu")
+	else:
+    	print("No sé que ruido hace este animal.")
+	print("Sólo los gatos maullan.")
